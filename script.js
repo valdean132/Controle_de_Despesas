fa8155052f2559a122523dc73f67400e43cddd36
valdean132 <65917804+valdean132@users.noreply.github.com>
2021-03-01 09:56:38 -0400

    Alterações

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let transactions = localStorage
 const removeTransaction = ID =>{
     transactions = transactions.filter(transaction => 
         transaction.id !== ID);
+    
     updateLocalStorage();
     init();
 }
